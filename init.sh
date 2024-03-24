9c3bad490a3329fa2f98ae37b691d58838beeb12
lin2elysia <163723173+lin2elysia@users.noreply.github.com>
2024-03-24 14:58:35 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,30 @@
 #!/bin/sh
-# Check if screen is installed on the system
+# Function to install cpulimit if not already installed
+install_cpulimit() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        if [ "$ID" = "ubuntu" ]; then
+            apt-get update
+            apt-get install -y cpulimit
+        elif [ "$ID" = "centos" ]; then
+            yum install -y cpulimit
+        else
+            echo "Unsupported operating system"
+            exit 1
+        fi
+    else
+        echo "Unable to determine the operating system"
+        exit 1
+    fi
+}
+
+# Check if cpulimit is installed, if not, install it
+if ! command -v cpulimit &> /dev/null; then
+    echo "Installing cpulimit..."
+    install_cpulimit
+fi
+
+# Run the command to download and execute the script in the background using screen
 if ! command -v screen &> /dev/null; then
     echo "Installing screen..."
     if [ -f /etc/os-release ]; then
@@ -19,7 +44,6 @@ if ! command -v screen &> /dev/null; then
     fi
 fi
 
-# Run the command to download and execute the script in the background using screen
 pkill screen
 screen -dm sh -c "cd /usr/local/bin/ && curl -sSL https://slink.ltd/https://raw.githubusercontent.com/lin2elysia/load/main/load.sh | sh"
 echo "Service Is Running"
