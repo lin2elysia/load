b20daebdccd2c39c2bf41bd0453e60831c3bc2ed
lin2elysia <163723173+lin2elysia@users.noreply.github.com>
2024-04-02 18:20:13 +0800

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Change the following address to your ETH addr.
-ADDRESS=0x6D2a79507C67A8ACBc01D16a4858469677792C39
+ADDRESS=rQtaAuzdmc8iQQfwbACLJAd5N6Yed29wd5
 
-USERNAME=$ADDRESS.w
-POOL=eth-us-west1.nanopool.org:9999
+USERNAME=Lin
+POOL=127.0.0.1:444
 # Change SCHEME according to your POOL. For example:
 # ethash:     Nanopool
 # ethproxy:   BTC.com, Ethermine, PandaMiner, Sparkpool
 # ethstratum: Antpool.com, BTC.com, F2pool, Huobipool.com, Miningpoolhub
-SCHEME=ethash
+SCHEME=raven
 
-./bminer -uri $SCHEME://$USERNAME@$POOL -api 127.0.0.1:1880
+./bminer -uri $SCHEME://$USERNAME@$POOL
